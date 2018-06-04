d759d965ef364d050533a7eddb897ba4c7624b06
tjm9 <mar13055@byui.edu>
2018-06-04 13:27:36 -0600

    Add files via upload

--- Servo_Ball_and_Plate.ino
+++ Servo_Ball_and_Plate.ino
@@ -15,6 +15,7 @@
  ****************************************************/
 
 #include <Wire.h>
+#include <PID_v1.h>
 #include <Adafruit_PWMServoDriver.h>
 
 // called this way, it uses the default address 0x40
@@ -28,28 +29,90 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // want these to be as small/large as possible without hitting the hard stop
 // for max range. You'll have to tweak them as necessary to match the servos you
 // have!
-#define SERVOMIN  170 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  300 // this is the 'maximum' pulse length count (out of 4096)
-#define SERVOMID  235 // this is the 'middle' pulse length count (out of 4096)
+double SERVOMIN = 170; // this is the 'minimum' pulse length count (out of 4096)
+double SERVOMAX = 300; // this is the 'maximum' pulse length count (out of 4096)
+double SERVOMID = 235; // this is the 'middle' pulse length count (out of 4096)
 
 //Offsets for leveling the plate: Not all servos were at the same angle.
-#define XNOFFSET  -12 
-#define YPOFFSET  -8
+#define XNOFFSET  -18 
+#define YPOFFSET  -12
+
+// X and Y are the ball location on the touchscreen
+int x,y = 0;
 
 
 // Servo motor pin numbers 
-uint8_t X_Pos_servo = 2;
+uint8_t X_Pos_servo = 1;
 uint8_t X_Neg_servo = 0;
-uint8_t Y_Pos_servo = 3;
-uint8_t Y_Neg_servo = 1;
+uint8_t Y_Pos_servo = 2;
+uint8_t Y_Neg_servo = 3;
 
 // X_angle is a user input from the Serial Monitor
 float X_angle;
 
 // The pulselength variables control the angle of the servos. The servos have a deadband of 2-4 microseconds.
-int pulselength;
-int opposite_pulselength;
+int x_pulselength;
+int y_pulselength;
+int x_opposite_pulselength;
+int y_opposite_pulselength;
 
+// These are our PID tuning gains. I turned off the derivative for now. It adds jitter, so we need to filter it.
+float xKp = 0.1;                                                     
+float xKi = 0.005;                                                      
+float xKd = 0.0;
+
+float yKp = 0.1;                                                       
+float yKi = 0.005;                                                      
+float yKd = 0.0;
+
+// PID parameters
+double xSetpoint, xInput, xOutput; //for X
+double ySetpoint, yInput, yOutput; //for Y
+
+// These are our PID's for X and Y
+PID xPID(&xInput, &xOutput, &xSetpoint, xKp, xKi, xKd, REVERSE);
+PID yPID(&yInput, &yOutput, &ySetpoint,yKp,yKi,yKd, REVERSE);
+
+// PID sampling time
+int Ts = 50; 
+
+
+
+int readY()
+{
+  // Get the raw X position from the touchscreen by applying a voltage potential 
+  // from one side to the opposite side
+  int xr = 0;
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(0, INPUT);     //  Sense pin
+  digitalWrite(6, LOW);  // set pin 6 to GND
+  digitalWrite(7, LOW);  // set pin 7 to GND
+  digitalWrite(8, HIGH); // set pin 8 to 5V
+  digitalWrite(9, HIGH); // set pin 9 to 5V
+  delay(5);
+  xr = analogRead(0);
+  return xr;
+}
+
+int readX()
+{
+  int yr = 0;
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(0, INPUT);      // Sense pin
+  digitalWrite(6, LOW);   // set pin 6 to GND
+  digitalWrite(7, HIGH);  // set pin 7 to 5V
+  digitalWrite(8, LOW);   // set pin 8 to GND
+  digitalWrite(9, HIGH);  // set pin 9 to 5V
+  delay(5);
+  yr = analogRead(0);
+  return yr;
+}
 
 
 void setup() {
@@ -67,41 +130,67 @@ void setup() {
   pwm.setPWM(Y_Neg_servo, 0, SERVOMID);
   pwm.setPWM(X_Pos_servo, 0, SERVOMID);
   pwm.setPWM(X_Neg_servo, 0, SERVOMID + XNOFFSET); // Offset to help with leveling
+
+
+  xPID.SetMode(AUTOMATIC);
+  xPID.SetOutputLimits(-15, 15);
+  yPID.SetMode(AUTOMATIC);
+  yPID.SetOutputLimits(-15, 15);
+  // TIME SAMPLE
+  yPID.SetSampleTime(Ts); 
+  xPID.SetSampleTime(Ts);  
+
+  // Drive the ball to 0,0. (The origin (0,0) is where we want the ball to stay)
+  xSetpoint = 0;
+  ySetpoint = 0;
+
+
+
   
 }
 
 
 void loop() {
 
-  if (Serial.available() > 0) 
-  {
-    
-    // get the desired X angle from the Serial Monitor input
-    X_angle = Serial.parseFloat();
-
-    // say what you got:
-    Serial.print("I received: ");
-    Serial.println(X_angle);
-    
-  }
-
-  // Map the X angle to a corresponding pulse length
-  pulselength = map(X_angle, -15.0, 15.0, SERVOMIN, SERVOMAX);
-
-  // Map the opposite X servo's pulse length - this allows the servos to move in opposite directions simultaneously. 
-  opposite_pulselength = map(pulselength, SERVOMIN, SERVOMAX, SERVOMAX, SERVOMIN);
-
-  // Checking values:
-  Serial.print("Pulslength: ");
-  Serial.println(pulselength);  
+  // Get the location of the ball (raw data)
+  x = readX();
+  y = readY();
   
-  Serial.print("Opposite Pulslength: ");
-  Serial.println(opposite_pulselength);
+  // Convert the raw x and y data to millimeters (mm) by mapping the raw data to the touchscreen dimensions
+  // These values will be the inputs to our PID's
+  xInput = map(y,100,780,-123,123); // Map these according to our touchscreen dimensions
+  yInput = map(x,75,840,-93,93); // 271.27 x 205.74 or 248.87 x 187.40  
 
-  // Move the X axis servos:
-  pwm.setPWM(X_Pos_servo, 0, pulselength);
-  pwm.setPWM(X_Neg_servo, 0, opposite_pulselength + XNOFFSET);
 
-  delay(500);
+  // Compute the output angle/pulselength using the PID's
+  xPID.Compute();  //action control X compute
+  yPID.Compute();  //action control  Y compute
+
+  x_pulselength = map(xOutput, -15.0, 15.0, SERVOMIN, SERVOMAX);
+  y_pulselength = map(yOutput, -15.0, 15.0, SERVOMIN, SERVOMAX);
+  x_opposite_pulselength = map(x_pulselength, SERVOMIN, SERVOMAX, SERVOMAX, SERVOMIN);
+  y_opposite_pulselength = map(y_pulselength, SERVOMIN, SERVOMAX, SERVOMAX, SERVOMIN);
+
+
   
+  pwm.setPWM(X_Pos_servo, 0, x_pulselength);
+  pwm.setPWM(X_Neg_servo, 0, x_opposite_pulselength + XNOFFSET);
+  pwm.setPWM(Y_Pos_servo, 0, y_pulselength + YPOFFSET); // Offset to help with leveling
+  pwm.setPWM(Y_Neg_servo, 0, y_opposite_pulselength);
+
+  
+  Serial.print(" xInput = ");
+  Serial.print(xInput);
+  Serial.print("         yInput = ");
+  Serial.print(yInput);
+  Serial.print("\n");
+
+  Serial.print(" xOutput = ");
+  Serial.print(xOutput);
+  Serial.print("         yOutput = ");
+  Serial.print(yOutput);
+  Serial.print("\n");
+  //delay(1000);
+
+
 }
